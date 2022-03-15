6dc70dfc110f979d6d2cd2580ac1d7a928344fd2
godan <matsushita@godan.xyz>
2022-03-16 01:42:58 +0900

    [add] 基本的なpomodoro機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Time :time="time_sec" />
-  <button v-on:click="startTimer" >start</button>
+  <button v-if="timer == null" v-on:click="startTimer" >start</button>
+  <button v-if="timer != null" v-on:click="stopTimer" >stop</button>
+  <button v-on:click="nextTimer" >next</button>
+  {{mode}}
 </template>
 
 <script>
@@ -14,20 +17,41 @@ export default {
   },
   data: () => {
     return {
-      time_sec: 1801
+      time_sec: 1801,
+      timer: null,
+      mode: 'work',
+      mode_type_list: {
+        work: 25 * 60,
+        rest: 5 * 60
+      }
 
-    }
+    } 
   },
   methods: {
      countdown: function() {
       if (this.time_sec >= 1) {
         this.time_sec--;
-      }
+      }``
     },
     startTimer: function() {
       this.timer = setInterval(() => this.countdown(), 1000);
       this.resetButton = true;
     },
+    stopTimer: function() {
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+    nextTimer: function(){
+      this.stopTimer();
+      if(this.mode == "work"){
+        this.time_sec = this.mode_type_list["rest"]
+        this.mode = "rest"
+      }
+      else if (this.mode == "rest") {
+        this.time_sec = this.mode_type_list["work"]
+        this.mode = "work"
+      }
+    }
   }
 }
 </script>
